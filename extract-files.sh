96b972e11b1a516ec3dc28d7b36f6e12a71ebdf5
Woomymy <woomy@woomy.be>
2023-11-12 13:20:42 +0100

    light: Add a shim to link sensor blobs against `android.hardware.sensors@1.0-convert`
    * `libndksensorbridge` was converted to AIDL and is no longer linked to
      this static library that contains symbols required by our sensor
      blobs. Add a shim linked to `android.hardware.sensors@1.0-convert` to
      fix our sensors
    
    Change-Id: I969f97bb15d0abcc71f87352722995db3826e122
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -105,6 +105,10 @@ function blob_fixup {
 	grep -q "libprotobuf-cpp-lite-3.9.1.so" "${2}" && \
 	"${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
 	;;
+        vendor/bin/mnld | vendor/lib*/libaalservice.so | vendor/lib*/libcam.utils.sensorprovider.so)
+	grep -q "libshim_sensors.so" "${2}" || \
+	"${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
+	;;
     esac
 }
 
